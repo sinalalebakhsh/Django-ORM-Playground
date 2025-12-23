4122de4cc902f86e7aa0968732cd751d9e8d8554
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2025-12-23 05:57:45 +0330

    ALL in one

--- orm_playground/playground/views.py
+++ orm_playground/playground/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
-
+from django.db.models import F
 # Create your views here.
+from decimal import Decimal, ROUND_HALF_UP
+
+from playground.models.product import Product
+
+
+
+
+
+def run_product_url(request):
+    """
+        You have to solve it manually.
+    """
+    for product in products:
+        product.price = product.price * Decimal("1.1")
+        product.save()
+    """
+        You have to solve it manually.
+    """
+    for product in products:
+        product.price = (product.price * Decimal("1.1")).quantize(
+            Decimal("0.01"),
+            rounding=ROUND_HALF_UP
+        )
+        product.save()
+
+
+    """
+
+    """
+    Product.objects.update(price=F("price") * 1.1)
+    
+    products = Product.objects.filter(category__name="Electronics")
+
+    return render(request, 'sina.html')
